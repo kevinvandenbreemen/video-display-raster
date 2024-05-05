d9e89ee205a9023b0d131188dfb170c4f424ef65
Kevin <kevin.vandenbreemen@gmail.com>
2024-05-04 17:10:52 -0700

    Update the path to include my correct github username

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/vandenbreemen/video-display-raster")
+            url = uri("https://maven.pkg.github.com/kevinvandenbreemen/video-display-raster")
             credentials {
                 username = System.getenv("USERNAME")
                 password = System.getenv("TOKEN")
